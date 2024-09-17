f2a3306515eecc4fddc70b9500a1f5ba066b773a
Xiong <300094299@fvtc.edu>
2024-09-17 14:16:57 -0500

    done with assign 3

--- site.js
+++ site.js
@@ -3,3 +3,24 @@ i.textContent = new Date()
 i.style.color = 'green'
 i.style.paddingLeft = '1em'
 i.style.fontFamily = 'Courier New'
+
+// Time of Day
+const hours = new Date().getHours() 
+/*
+const morning = hours >= 4 && hours<12
+const afternoon = hours >= 12 && hours < 17 
+const evening = hours >= 17 || hours < 4 
+*/
+
+if ( hours >= 4 && hours<12 ) {
+    const hi = document.getElementById( 'welcome' )
+    hi.textContent = "Good Morning"
+}
+else if ( hours >= 12 && hours < 17  ) {
+    const hi = document.getElementById( 'welcome' )
+    hi.textContent = "Good Afternoon"
+}
+else { 
+    const hi = document.getElementById( 'welcome' )
+    hi.textContent = "Good Evening"
+}
